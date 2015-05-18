875c25bf5330d8be5c47c1569095a430d18ff16a
Emilia Paulski <epaulski@mail.depaul.edu>
2015-05-18 10:08:51 -0500

    updated translator file

--- src/CCtranslator.java
+++ src/CCtranslator.java
@@ -1,54 +1,28 @@
 //this is a wrapper that allows Languages to be specified as strings instead of as Languages only
 //Creates a translator object that is linked to Microsoft translation service 
-//Created by Colin Cruse 4/15
-
-
-// test
-
-
-import java.util.HashMap;
-import java.util.Map;
+//Created by Colin Cruse & Emilia Paulski
 
 import com.memetix.mst.language.Language;
 import com.memetix.mst.translate.Translate;
 
-
 public class CCtranslator {
-
-
-
-
-
-
-	  Map<String, Language> langs;
+	  
 	  Language from = null;
 	  Language to = null;
-	
 	
 	  CCtranslator(String id, String sec){
 		  Translate.setClientId(id);
 		  Translate.setClientSecret(sec);
-		  langs = new HashMap<String, Language>();
-		  
-		  //setting up dictionary of supported languages
-		  langs.put("English",Language.ENGLISH );
-		  langs.put("French",Language.FRENCH);
-		  langs.put("Arabic",Language.ARABIC );
-		  langs.put("Italian",Language.ITALIAN );
-		 
-		  
-		  //langs.put("English",Language.ENGLISH );
-		  //langs.put("English",Language.ENGLISH );  
 	  }
+	  
 	  //set language translating from
-	  public void setFromLang(String lang){
-		  this.from = langs.get(lang);
-		  
+	  public void setFromLang(Language lang){
+		this.from = lang;
 	  }
+	  
 	  //set language translating to
-	  public void setToLang(String lang){
-		  this.to = langs.get(lang);
-		  
+	  public void setToLang(Language lang){
+		  this.to = lang;
 	  }
 	  
 	  public String translate(String f){
@@ -56,11 +30,9 @@ public class CCtranslator {
 		  try {
 			translatedText = Translate.execute(f,this.from,this.to);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return translatedText;
-		  
 	  }
 	
 }
---
+++ src/Translator.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+import com.memetix.mst.language.Language;
+
+public class Translator {
+		
+	public static void listLanguages() throws Exception{
+		System.out.println("\n---\nLanguages available: \n");
+        for(Language lang : Language.values())
+            System.out.println(lang.name());
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		CCtranslator t = new CCtranslator("TranslatingMessenger","YY4h0AMA9M4q/3Ty52plCUCU998eE3qHAmwk/7Y8P4c=");
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Welcome to the Translating Messenger");
+		
+		//System.out.print("\nEnter your username: ");
+		//String username = scan.nextLine();
+		
+		System.out.print("Enter your language (type -o for options): ");
+		String lang1 = scan.nextLine();
+		if (lang1.equals("-o")){
+			listLanguages();
+			System.out.print("\nEnter your language (type -o for options): ");
+			lang1 = scan.nextLine();
+		}
+		t.setFromLang(Language.valueOf(lang1.toUpperCase()));
+		System.out.println("Your language: " + t.from.name());
+		
+		t.setToLang(Language.SPANISH); //temporary
+		
+		System.out.println("\n[User] has entered the conversation \nTheir language: " + t.to.name()); //translate!!!!!!
+		
+		System.out.print("\n[You]: "); 
+		String s = scan.nextLine();
+		String r = t.translate(s);
+		System.out.println(r);
+		
+		scan.close();
+	}
+
+}
