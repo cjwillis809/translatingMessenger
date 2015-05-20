f25d23c037c07866360d16999c32e6e5ee8e8421
Colin <ccruse@mail.depaul.edu>
2015-05-19 19:02:13 -0500

    first commit of my branch

--- src/Translator.java
+++ src/Translator.java
@@ -12,14 +12,15 @@ public class Translator {
 	
 	public static void main(String[] args) throws Exception {
 		
+		
 		CCtranslator t = new CCtranslator("TranslatingMessenger","YY4h0AMA9M4q/3Ty52plCUCU998eE3qHAmwk/7Y8P4c=");
 		
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.println("Welcome to the Translating Messenger");
 		
-		//System.out.print("\nEnter your username: ");
-		//String username = scan.nextLine();
+		System.out.print("\nEnter your username: ");
+		String username = scan.nextLine();
 		
 		System.out.print("Enter your language (type -o for options): ");
 		String lang1 = scan.nextLine();
